9633bef16e903bdd6d159e1a6181fc58947b4afc
durga-ds <63601392+durga-ds@users.noreply.github.com>
2020-09-07 17:38:35 -0400

    Create running_average.py

---
+++ Reusable-code/running_average.py
@@ -0,0 +1,22 @@
+# When given a list, calculate the "Running Average" for each item in the list and return a 
+# list of averages rounded to the nearest integer
+#Example: Calculate running averages for 
+#test_list = [3,2,4]
+#For 3 = (2+4)/2 = 3
+#For 2 = 4
+#For 4 = 0
+#print (get_avg(test_list))
+#[3, 4, 0]
+
+from statistics import mean 
+def get_avg(input_list):
+    output_list = []
+    l = len(input_list)
+    n = 1
+    while n < l:
+        output_list.append(round(mean(input_list[n:])))
+        n += 1
+    else: # if n == l:
+        output_item = 0
+        output_list.append(output_item)
+    return output_list
