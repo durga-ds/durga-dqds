d0f32850c6a2801c011bdce807c98e63ab3e0fc8
durga-ds <63601392+durga-ds@users.noreply.github.com>
2020-09-14 14:12:45 -0400

    Update pandas_read_rows.py

--- Reusable-code/pandas_read_rows.py
+++ Reusable-code/pandas_read_rows.py
@@ -1,8 +1,7 @@
-#Function to read given a csv file, using default encoding and print the specified number of lines from the dataframe.
-#Similar to pandas df.head()
+#Function to read given a csv file, using default encoding and print the specified number of lines from the DataFrame.
 #Example: pdread_csv(csvfile_name, 5) 
 
 import pandas as pd
 def pdread_csv(csvfile, numrows, enc='ISO-8859-1'):
-    rows = pd.read_csv( filename, nrows=numrows, encoding=enc)
+    rows = pd.read_csv(csvfile, nrows=numrows, encoding=enc)
     return rows
