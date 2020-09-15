713a1de69faca41a1685ed9d24cdc38dd21ba85f
durga-ds <63601392+durga-ds@users.noreply.github.com>
2020-09-15 12:09:24 -0400

    Update add_column_toCSV.py

--- Reusable-code/add_column_toCSV.py
+++ Reusable-code/add_column_toCSV.py
@@ -1,10 +1,9 @@
-# Python function to add a list of values as a new column to an existing CSV file and write desired result to an output file
+# Python function to add a new column to an existing CSV file from a list. Result is written to an output file
 #Example: Add values from [4,4,5,5,9,9,9] as a new column to `example_csv.csv`. Output to file `new_col.csv`
 #col_list = [4,4,5,5,9,9,9]
 #add_col_to_csv('example_csv.csv','new_col.csv',col_list)
 
 import csv
-
 def add_col_to_csv(csvfile,fileout,new_list):
     with open(csvfile, 'r') as read_f, \
         open(fileout, 'w', newline='') as write_f:
